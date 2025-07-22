f5ebdd5617540224806c68df06b146892977e8f2
tebras <km-tebenkoff@yandex.ru>
2025-07-22 21:03:06 +0800

    fix

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,16 @@
-spisok = [{'id': 41428829, 'satet': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+def filter_by_state(dictionaries: list, state: str) -> list:
+    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state соответствует указанному значению"""
 
-def filter_by_state(spisok) :
-    new_spisok_ex = []
-    new_spisok_can = []
-    for slovar in spisok:
-        for value in slovar.values():
-            if value == 'EXECUTED':
-                new_spisok_ex.append(slovar)
-            if value == 'CANCELED':
-                new_spisok_can.append(slovar)
+    new_dictionaries = []
+    for dictionary in dictionaries:
+        if dictionary.get("state") == state:
+            new_dictionaries.append(dictionary)
+    return new_dictionaries
 
-    return new_spisok_ex , new_spisok_can
 
-x=filter_by_state(spisok)
-print(x)
+def sort_by_date(dictionaries_data: list) -> list:
+    """Функция возвращает новый список, отсортированный по дате"""
+
+    sorted_dictionaries_data = sorted(dictionaries_data, key=lambda x: x["date"], reverse=True)
+    return sorted_dictionaries_data
