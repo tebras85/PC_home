08f045c51115518608c7ea9d2026e03bc2e060a7
tebras <km-tebenkoff@yandex.ru>
2025-07-24 21:59:44 +0800

    test_processing_3.1

--- src/processing.py
+++ src/processing.py
@@ -14,3 +14,21 @@ def sort_by_date(dictionaries_data: list) -> list:
 
     sorted_dictionaries_data = sorted(dictionaries_data, key=lambda x: x["date"], reverse=True)
     return sorted_dictionaries_data
+
+
+if __name__ == '__main__':
+
+    dictionaries = ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+    print(filter_by_state(dictionaries, state='EXECUTED'))
+    print(filter_by_state(dictionaries, state='CANCELED'))
+
+    dictionaries_data = ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+
+
+    print(sort_by_date(dictionaries_data))
---
+++ tests/test_processing.py
@@ -0,0 +1,20 @@
+import pytest
+
+from src.processing import filter_by_state,sort_by_date
+
+@pytest.mark.parametrize('dic, expected', [([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}],[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]),
+                                           ([{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}],[{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+                                           ([{'id': 939719570, 'state': 'EXECUTED', 'date': '2017-06-30T02:08:58.425572'}],[{'id': 939719570, 'state': 'EXECUTED', 'date': '2017-06-30T02:08:58.425572'}])])
+
+
+
+def test_filter_by_state_ex(dic, expected):
+    assert filter_by_state(dic,state ='EXECUTED') == expected
+
+@pytest.mark.parametrize('dic, expected', [([{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}],[{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}]),
+                                           ([{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],[{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]),
+                                           ([{'id': 615964591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],[{'id': 615964591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])])
+
+def test_filter_by_state_can(dic, expected):
+    assert filter_by_state(dic,state ='CANCELED') == expected
+
