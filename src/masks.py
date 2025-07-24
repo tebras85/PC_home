131277d81735e6021cd022886d750a1684484b26
tebras <km-tebenkoff@yandex.ru>
2025-07-24 18:48:57 +0800

    test_masks_1.2

--- src/masks.py
+++ src/masks.py
@@ -10,6 +10,8 @@ def get_mask_account(card_account: str) -> str:
     """
     Выводим последние цифры номера счета клиента
     """
+    if len(card_account) == 0:
+        return 'не корректно введены данные!!!'
     card_account = card_account.replace(" ", "")
     last_part = str(card_account[-4:])
     return f"**{last_part}"
@@ -19,3 +21,5 @@ if __name__ == '__main__':
 
     print(get_mask_card_number('123456'))
     print(get_mask_card_number('1111222233334444'))
+    print(get_mask_account('1111222233334444'))
+    print(get_mask_account(''))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,5 @@
 import pytest
-from src.masks import get_mask_card_number
+from src.masks import get_mask_card_number, get_mask_account
 
 @pytest.mark.parametrize ('card_number, expected' , [('7000792289606361' , '7000 79** **** 6361'),
                                                      ('7000792289606745', '7000 79** **** 6745'),
@@ -16,3 +16,15 @@ def test_masks(card_number, expected):
 
 def test_masks_type(card_number):
     assert get_mask_card_number(card_number) ==  'не корректно введены данные!!!'
+
+
+@pytest.mark.parametrize ('account, expected' , [('7000792289606361' , '**6361'),
+                                                     ('7000792289606745', '**6745'),
+                                                     ('7220792289606798', '**6798')
+                                                     ])
+
+def test_get_mask_account(account, expected):
+    assert get_mask_account(account) == expected
+
+def test_get_mask_account_zero():
+    assert get_mask_card_number('') ==  'не корректно введены данные!!!'
