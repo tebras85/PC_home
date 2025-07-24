72b258cccb5a70fc8f98138c1f3da0d5c6b50864
tebras <km-tebenkoff@yandex.ru>
2025-07-24 23:22:21 +0800

    test_all

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,53 +1,101 @@
 import pytest
 
-from src.processing import filter_by_state,sort_by_date
-
-@pytest.mark.parametrize('dic, expected', [([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}],[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]),
-                                           ([{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}],[{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
-                                           ([{'id': 939719570, 'state': 'EXECUTED', 'date': '2017-06-30T02:08:58.425572'}],[{'id': 939719570, 'state': 'EXECUTED', 'date': '2017-06-30T02:08:58.425572'}]),
-                                           ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-                                            [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-                                            ])])
+from src.processing import filter_by_state, sort_by_date
 
 
+@pytest.mark.parametrize(
+    "dic, expected",
+    [
+        (
+            [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}],
+            [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}],
+        ),
+        (
+            [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}],
+            [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}],
+        ),
+        (
+            [{"id": 939719570, "state": "EXECUTED", "date": "2017-06-30T02:08:58.425572"}],
+            [{"id": 939719570, "state": "EXECUTED", "date": "2017-06-30T02:08:58.425572"}],
+        ),
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+    ],
+)
+def test_filter_by_state_ex(dic: list, expected: list) -> list:
+    """
+    тест по ключу state
+    """
+    assert filter_by_state(dic, state="EXECUTED") == expected
 
-def test_filter_by_state_ex(dic: list , expected: list)-> list:
-    assert filter_by_state(dic,state ='EXECUTED') == expected
 
-@pytest.mark.parametrize('dic, expected', [([{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}],[{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}]),
-                                           ([{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],[{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]),
-                                           ([{'id': 615964591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],[{'id': 615964591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]),
-                                           ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-                                            [{'id': 594226727, 'state': 'CANCELED','date': '2018-09-12T21:27:25.241689'},
-                                            {'id': 615064591, 'state': 'CANCELED','date': '2018-10-14T08:21:33.419441'}]
-                                            )])
+@pytest.mark.parametrize(
+    "dic, expected",
+    [
+        (
+            [{"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}],
+            [{"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"}],
+        ),
+        (
+            [{"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}],
+            [{"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}],
+        ),
+        (
+            [{"id": 615964591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}],
+            [{"id": 615964591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}],
+        ),
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+    ],
+)
+def test_filter_by_state_can(dic: list, expected: list) -> list:
+    """
+    тест по ключу state
+    """
+    assert filter_by_state(dic, state="CANCELED") == expected
 
-def test_filter_by_state_can(dic: list , expected: list)-> list :
-    assert filter_by_state(dic,state ='CANCELED') == expected
 
-@pytest.mark.parametrize('dat, expected', [(
-                          [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-                          [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])
-])
-
-def test_sort_by_date(dat: list, expected: list )-> list:
+@pytest.mark.parametrize(
+    "dat, expected",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        )
+    ],
+)
+def test_sort_by_date(dat: list, expected: list) -> list:
+    """
+    тест сортировка по дате
+    """
     assert sort_by_date(dat) == expected
-
-
-
-
-
-
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -11,36 +11,33 @@ from src.widget import get_date, mask_accoud_card
         ("Master Card 7000792289606300", "Master Card 7000 79** **** 6300"),
         ("Счет 73654108430135874305", "Счет **4305"),
         ("Счет 73654108430135874399", "Счет **4399"),
-    ]
+    ],
 )
-def test_mask_accoud_card(card_number: str, expected: str)-> str:
+def test_mask_accoud_card(card_number: str, expected: str) -> str:
     """
     тест корректности маскировки карты
     """
     assert mask_accoud_card(card_number) == expected
 
 
-@pytest.mark.parametrize("card_number",
-                         [("111222233334444"),
-                     ("70007922896067450"),
-                     ("722079228960679"),
-                     ("32432")]
-)
+@pytest.mark.parametrize("card_number", [("111222233334444"), ("70007922896067450"), ("722079228960679"), ("32432")])
 def test_mask_accoud_card_n(card_number):
     """
     тест корректности ввода номера карты 16 символов
     """
-    assert mask_accoud_card(card_number) == ' не корректно введены данные!!!'
+    assert mask_accoud_card(card_number) == " не корректно введены данные!!!"
 
-@pytest.mark.parametrize('data, expected' ,[("2024-03-11T02:26:18.671407","11.03.2024"),
-                                              ("2023-03-11T02:26:18.671407","11.03.2023"),
-                                            ("2025-04-11T02:26:00.671407","11.04.2025")])
 
-def test_get_date(data: str ,expected: str)-> str:
-    '''
+@pytest.mark.parametrize(
+    "data, expected",
+    [
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+        ("2023-03-11T02:26:18.671407", "11.03.2023"),
+        ("2025-04-11T02:26:00.671407", "11.04.2025"),
+    ],
+)
+def test_get_date(data: str, expected: str) -> str:
+    """
     тест корректности даты
-    '''
+    """
     assert get_date(data) == expected
-
-
-
