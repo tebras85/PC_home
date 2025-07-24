1d7788702c7b1795b42f5ff28c5cb369273f0760
tebras <km-tebenkoff@yandex.ru>
2025-07-24 18:37:35 +0800

    test_masks_1.1

--- src/masks.py
+++ src/masks.py
@@ -3,9 +3,7 @@ def get_mask_card_number(card_number: str) -> str:
     Делаем маску номера карты , скрывая некоторые цифры номера карты.
     """
     if len(card_number) < 16 or len(card_number) > 16:
-        return 'Не коректный вод карты!!! '
-    #formatted_number = card_number[:4] + " " + card_number[4:6] + "** ****" + " " + card_number[-4:]
-    #return formatted_number
+        return  'не корректно введены данные!!!'
     return card_number[:4] + " " + card_number[4:6] + "** ****" + " " + card_number[-4:]
 
 def get_mask_account(card_account: str) -> str:
@@ -15,3 +13,9 @@ def get_mask_account(card_account: str) -> str:
     card_account = card_account.replace(" ", "")
     last_part = str(card_account[-4:])
     return f"**{last_part}"
+
+
+if __name__ == '__main__':
+
+    print(get_mask_card_number('123456'))
+    print(get_mask_card_number('1111222233334444'))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -7,3 +7,12 @@ from src.masks import get_mask_card_number
                                                      ])
 def test_masks(card_number, expected):
     assert get_mask_card_number(card_number) == expected
+
+@pytest.mark.parametrize ('card_number,' , [('70007922896063610'),
+                                            ('70007922896067450'),
+                                            ('722079228960679'),
+                                            ('32')
+                                            ])
+
+def test_masks_type(card_number):
+    assert get_mask_card_number(card_number) ==  'не корректно введены данные!!!'
